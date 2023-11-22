cd3a79d6b13fab8bfdf5ac6d8b09cd944f42d768
Your Name <you@example.com>
2023-11-21 22:59:01 -0500

    new1

--- static/logout.js
+++ static/logout.js
@@ -2,7 +2,7 @@ function logOut() {
     // Clear session storage
     sessionStorage.clear();
     // Redirect to the logged-out page
-    window.location.href = "http://a9a0b8caab82d452e9e195cd3b82ceca-2029017589.us-east-1.elb.amazonaws.com/logged-out";
+    window.location.href = "http://a93a9b6a6f55d4d5b84e861039f6ddc1-1492173285.us-east-1.elb.amazonaws.com/logged-out";
   }
 
   // Add click event listener for the logout button
